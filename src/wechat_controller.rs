1eaec7f172fc4037e75f60a2fb1bfa78e94f0727
StrayMeteor3337 <158819085+StrayMeteor3337@users.noreply.github.com>
2025-10-01 14:49:20 +0800

    V2.0.0版本(未完成)，已完成群聊选择功能，并修改了原来的大部分检测逻辑

--- src/wechat_controller.rs
+++ src/wechat_controller.rs
@@ -1,10 +1,14 @@
+use serde_json::json;
+
 const WEHCAT_API:&str = "http://127.0.0.1:{port}/api/?type={type}";
 
 const WECHAT_SET_VERSION:u8 = 35; //修改微信版本号
 const WECHAT_CHECK_LOGIN:u8 = 0; //检测微信登录状态
 const WECHAT_GET_PROFILE:u8 = 1; //获取个人信息
 const WECHAT_GET_CONTACT:u8 = 15;//获取微信联系人列表
-const WECHAT_CHECK_RELATION:u8 = 16;//查询微信好友关系
+const WECHAT_CHATROOM_ADD_MEMBER:u8 = 28;//添加群成员
+const WECHAT_CHATROOM_DEL_MEMBER:u8 = 27;//删除群成员
+const WECHAT_CHATROOM_GET_MEMBER:u8 = 25;//查询群成员列表
 const WECHAT_SET_REMARK:u8 = 24;//给异常好友添加备注
 
 /// 修改微信版本号,避免登录的时候提示版本过低
@@ -91,22 +95,18 @@ pub async fn get_wechat_contact(port:u16) -> Result<serde_json::Value, reqwest::
     }
 }
 
-/// 查询微信好友关系
-/// 接口返回值的status的意义:
-/// * `176(0xB0)`：被对方删除
-/// * `177(0xB1)`：正常好友
-/// * `178(0xB2)`：对方账号状态异常
-/// * `181(0xB5)`：被对方拉黑
-pub async fn check_relation(port:u16,wxid:&str) -> Result<serde_json::Value, reqwest::Error> {
+/// 获取群成员列表
+pub async fn chatroom_get_member(port:u16,chatroom_id:&str) -> Result<serde_json::Value, reqwest::Error>
+{
     let client = reqwest::Client::new();
     let url = WEHCAT_API
         .replace("{port}", &port.to_string())
-        .replace("{type}", &WECHAT_CHECK_RELATION.to_string());
+        .replace("{type}", &WECHAT_CHATROOM_GET_MEMBER.to_string());
     
     let response = client
         .post(&url)
         .json(&serde_json::json!({
-            "wxid": wxid
+            "chatroom_id": chatroom_id
         }))
         .send()
         .await?;
@@ -117,6 +117,70 @@ pub async fn check_relation(port:u16,wxid:&str) -> Result<serde_json::Value, req
     } else {
         Err(response.error_for_status().unwrap_err())
     }
+}
+
+/// 将好友添加到群聊
+pub async fn chatroom_add_member(port:u16,chatroom_id:&str,wxids:&[&str]) -> Result<serde_json::Value, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let url = WEHCAT_API
+        .replace("{port}", &port.to_string())
+        .replace("{type}", &WECHAT_CHATROOM_ADD_MEMBER.to_string());
+    
+    let response = client
+        .post(&url)
+        .json(&serde_json::json!({"chatroom_id":chatroom_id,
+            "wxids":wxids
+        }))
+        .send()
+        .await?;
+    
+    if response.status().is_success() {
+        let json: serde_json::Value = response.json().await?;
+        Ok(json)
+    } else {
+        Err(response.error_for_status().unwrap_err())
+    }
+}
+
+/// 将好友移出群聊
+pub async fn chatroom_del_member(port:u16,chatroom_id:&str,wxids:&[&str]) -> Result<serde_json::Value, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let url = WEHCAT_API
+        .replace("{port}", &port.to_string())
+        .replace("{type}", &WECHAT_CHATROOM_DEL_MEMBER.to_string());
+    
+    let response = client
+        .post(&url)
+        .json(&serde_json::json!({"chatroom_id":chatroom_id,
+            "wxids":wxids
+        }))
+        .send()
+        .await?;
+    
+    if response.status().is_success() {
+        let json: serde_json::Value = response.json().await?;
+        Ok(json)
+    } else {
+        Err(response.error_for_status().unwrap_err())
+    }
+}
+
+
+/// 查询微信好友关系
+/// 函数返回值: 
+/// ### 包含所有异常好友的向量
+pub async fn check_relation(port:u16,chatroom_id:&str,wxids:&[&str]) -> Result<serde_json::Value, ()> {
+    let response_add_member = match chatroom_add_member(port, chatroom_id, wxids).await {
+        Ok(res) => res,
+        Err(_) => return Err(()),
+    };
+    if response_add_member["result"] != "OK" {
+        return Err(());
+    }
+    //查询群成员列表判断是否有异常好友
+
+
+    Ok(json!({"delete":[],"block":[]}))
 }
 
 pub async fn set_remark(port:u16,wxid:&str,remark:&str) -> Result<serde_json::Value, reqwest::Error> {
